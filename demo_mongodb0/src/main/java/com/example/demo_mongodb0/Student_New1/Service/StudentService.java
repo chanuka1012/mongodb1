3b631f3c87dc37cfdfe64ae2a7ff9f1f71d19016
chanuka1012 <chanukarathnayake1012@gmail.com>
2024-12-13 00:01:00 +0530

    first comminjht

--- demo_mongodb0/src/main/java/com/example/demo_mongodb0/Student_New1/Service/StudentService.java
+++ demo_mongodb0/src/main/java/com/example/demo_mongodb0/Student_New1/Service/StudentService.java
@@ -1,5 +1,24 @@
 package com.example.demo_mongodb0.Student_New1.Service;
 
+import java.util.List;
+
+import com.example.demo_mongodb0.Student_New1.Model.Student;
+
 public interface StudentService {
 
+    Student saveStudent(Student student);
+
+    List<Student> getAllStudents();
+
+    Student getStudentById(String id);
+
+    Student updateStudent(Student student,String id);
+
+    void deleteStudent(String id);
+    
+    List<Student> getStudentByYearOfEnrollment(int yearOfEnrollment);
+
+    String getDepartmentById(String id);
+
+    void removeStudentByYearOfEnrollment(int yearOfEnrollment);
 }
